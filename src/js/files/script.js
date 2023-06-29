995ce0de0a1cd266525e061f70298b5388ff1815
Hunter111 <sukhomudrenkov17@gmail.com>
2023-06-29 16:06:21 +0300

    I have changed picture and description on meta tag

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,14 @@
 import {isMobile} from './functions.js';
 // Підключення списку активних модулів
 import {flsModules} from './modules.js';
+
+var videos = document.getElementsByClassName('.highlights-video-slide__video');
+
+for (var i = 0; i < videos.length; i++) {
+	var video = videos[i];
+	var playBtn = video.getElementsByClassName('play-btn')[0];
+
+	video.addEventListener('click', function () {
+		playBtn.classList.add('hide-play-btn');
+	});
+}
